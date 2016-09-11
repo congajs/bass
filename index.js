37b565ddc6f3a50cbbe717d371c9596fa06a344f
Marc Roulias <marc@marcroulias.com>
2016-09-11 02:28:03 +0000

    ported everything to ES6

---
+++ index.js
@@ -0,0 +1,58 @@
+module.exports = {
+	Bass: require('./lib/bass.js')
+};
+
+// /**
+//  * The repository class exposed for inheritance
+//  */
+// Bass.Repository = require('./repository');
+
+// /**
+//  * The Query class exposed
+//  */
+// Bass.Query = require('./query');
+
+// /**
+//  * The QueryResult class exposed
+//  */
+// Bass.QueryResult = require('./query-result');
+
+// /**
+//  * The base Connection for all adapters to extend
+//  */
+// Bass.Connection = require('./connection');
+
+// /**
+//  * The ManagerDefinition class exposed
+//  */
+// Bass.ManagerDefinition = require('./manager-definition');
+
+// *
+//  * The MetadataRegistry class exposed
+ 
+// Bass.MetadataRegistry = require('./metadata-registry');
+
+// /**
+//  * The Metadata class exposed
+//  */
+// Bass.Metadata = require('./mapping/metadata');
+
+// /**
+//  * The Mapper class exposed
+//  */
+// Bass.Mapper = require('./mapper');
+
+// /**
+//  * The UnitOfWork class exposed
+//  */
+// Bass.UnitOfWork = require('./unit-of-work');
+
+// /**
+//  * The DocumentCache class exposed
+//  */
+// Bass.DocumentCache = require('./document-cache');
+
+// /**
+//  * The IdStrategy class exposed
+//  */
+// Bass.IdStrategy = require('./id-strategy');
